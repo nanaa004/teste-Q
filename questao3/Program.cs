ef0a93b05918494350872acc81776b76bd6cbc3f
nanaa004 <fernandaffranca004@gmail.com>
2025-04-24 09:05:59 -0300

    foi

---
+++ questao3/Program.cs
@@ -0,0 +1,13 @@
+﻿string usuario = "admin";
+
+Console.WriteLine("Informe qual seu cargo: ");
+usuario = Console.ReadLine();
+
+if (usuario == "admin" || usuario == "supervisor")
+{
+    Console.WriteLine("Acesso concedido");
+}
+else
+{
+    Console.WriteLine(":(");
+}
---
+++ questao4/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int idade = 66;
+        double renda = 1900.0;
+
+     
+
+        if (idade >= 66 || renda >= 1900.0);
+        {
+            Console.WriteLine("Está Isento");
+        }
+    }
+}
